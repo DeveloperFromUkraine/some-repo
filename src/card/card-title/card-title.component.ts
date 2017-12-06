2c6ef0e421e1101eba45fe62d3187f4152a1a1a7
zhanl <zhan_liang@ultimatesoftware.com>
2017-12-06 11:34:31 -0500

    fix long input

--- src/card/card-title/card-title.component.ts
+++ src/card/card-title/card-title.component.ts
@@ -3,7 +3,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'ign-card-title',
   templateUrl: './card-title.html',
-  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./card-title.scss'],
 })
 export class CardTitleComponent {}
