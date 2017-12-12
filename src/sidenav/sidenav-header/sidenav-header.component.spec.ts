f8e34d9cb689b5f5316f008584cd9d3556134d6d
Brian Mahecha <Brian_Mahecha@ultimatesoftware.com>
2017-12-12 09:38:53 -0500

    completed integration tests for SideNavHeaderComponent

---
+++ src/sidenav/sidenav-header/sidenav-header.component.spec.ts
@@ -0,0 +1,57 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SideNavHeaderComponent } from 'index';
+import { DebugElement } from '@angular/core/src/debug/debug_node';
+import { ComponentTest } from '../../../test/test-bed/component';
+import { By } from '@angular/platform-browser';
+
+describe('SideNavHeaderComponent', () => {
+    let fixture: ComponentFixture<SideNavHeaderComponent>;
+    let component: SideNavHeaderComponent;
+    let de: DebugElement;
+    let ne: HTMLElement;
+
+    beforeEach(async () => {
+        await ComponentTest.createTestBed([], [SideNavHeaderComponent]);
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SideNavHeaderComponent);
+        component = fixture.componentInstance;
+
+        fixture.detectChanges();
+    });
+
+    it('should set title', async () => {
+        de = fixture.debugElement.query(By.css('.name'));
+        ne = de.nativeElement;
+
+        component.title = 'test';
+        await fixture.detectChanges();
+
+        expect(ne.textContent).toContain(component.title);
+    });
+
+    it('should not set title if no title set', () => {
+        de = fixture.debugElement.query(By.css('.name'));
+        ne = de.nativeElement;
+
+        expect(ne.textContent).not.toContain(component.title);
+    });
+
+    it('should set subtitle', async () => {
+        de = fixture.debugElement.query(By.css('.position'));
+        ne = de.nativeElement;
+
+        component.subtitle = 'test';
+        await fixture.detectChanges();
+
+        expect(ne.textContent).toContain(component.subtitle);
+    });
+
+    it('should not set subtitle if no subtitle set', () => {
+        de = fixture.debugElement.query(By.css('.position'));
+        ne = de.nativeElement;
+
+        expect(ne.textContent).not.toContain(component.subtitle);
+    });
+});
