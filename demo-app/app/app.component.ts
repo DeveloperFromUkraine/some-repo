1c68d2c407a1c8dd8dea9ff39d0ca8b1b33f5407
Derek Stobbe <djstobbe@gmail.com>
2018-01-08 13:32:01 -0700

    Start moving confluence docs to the docsite

--- demo-app/app/app.component.ts
+++ demo-app/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: [
+    './app.component.scss'
+  ],
 })
-export class AppComponent {
-  myMarkdown = "# test\n\nhello *world*";
+export class AppComponent implements AfterViewInit {
+  @ViewChild('header')
+  header: ElementRef;
+  headerHeight: number;
+
+  navItems = [
+    { name: 'Dialog', route: '/dialog' },
+    { name: 'Radio Button', route: '/radio-button' },
+    { name: 'Checkbox', route: '/checkbox' },
+  ];
+
+  ngAfterViewInit () {
+    setTimeout(() => {
+      this.headerHeight = this.header.nativeElement.offsetHeight;
+    });
+  }
 }
---
+++ demo-app/app/demo/demo-radio-button/demo-radio-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+const NOTES: string = require('raw-loader!./demo-radio-button.md');
+
+@Component({
+  selector: 'demo-radio-button',
+  templateUrl: './demo-radio-button.html',
+})
+export class DemoRadioButtonComponent {
+  notes = NOTES;
+}
