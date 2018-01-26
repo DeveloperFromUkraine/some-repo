80f2301e0a4944080e767903881c370168d0af86
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2018-01-26 15:55:09 -0500

    New field component: ign-field-inline

--- src/field/field.component.spec.ts
+++ src/field/field.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FieldComponent } from './field.component';
+import {FieldComponent, FieldInlineComponent} from "./field.component";
 import { ComponentTest } from '../../test/test-bed/component';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
@@ -36,5 +36,33 @@ describe('FieldComponent', () => {
         await fixture.detectChanges();
 
         expect(fixture).toMatchSnapshot();
+    });
+});
+
+describe('FieldInlineComponent', () => {
+    let fixture: ComponentFixture<FieldInlineComponent>;
+    let component: FieldInlineComponent;
+    let de: DebugElement;
+    let ne: HTMLElement;
+
+    beforeEach(async () => {
+        ComponentTest.createTestBed([], [FieldInlineComponent]);
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FieldInlineComponent);
+        component = fixture.componentInstance;
+
+        fixture.detectChanges();
+    });
+
+    it('should set label input', async () => {
+        de = fixture.debugElement.query(By.css('.label'));
+        ne = de.nativeElement;
+
+        component.label = 'label!';
+        await fixture.detectChanges();
+
+        expect(ne.textContent).toContain(component.label);
     });
 });
