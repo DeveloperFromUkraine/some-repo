2f3d3a0bde3832e2aa8df4ddb2fec6322d9e74bd
Mariah <mariah_avalos@ultimatesoftware.com>
2018-02-08 17:15:29 -0500

    adding functionality for slackbot

--- demo-app/app/app.component.ts
+++ demo-app/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, ElementRef, ViewChild, OnInit, OnDestroy} from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit, OnDestroy, Inject} from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatDialog } from '@angular/material';
-import {Routes} from '@angular/router';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { Routes, ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { DemoContributionComponent } from './demo/demo-contribution/demo-contribution.component';
-import {Subject} from 'rxjs/Subject';
+import { Subject } from 'rxjs/Subject';
+import { SlackService } from './services/slack-service'
+import { Response } from '@angular/http';
 
 const routes: Routes = [
     { path: 'contribution', component: DemoContributionComponent},
@@ -14,11 +16,12 @@ const routes: Routes = [
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     @ViewChild('header')
     header: ElementRef;
     headerHeight: number;
-
+    private router$;
+    private currUrl: string;
   navItems = [
     { name: 'Accessibility', route: '/accessibility-component' },
     { name: 'Card', route: '/card' },
@@ -47,23 +50,33 @@ export class AppComponent {
     { name: 'Sidenav', route: '/sidenav' },
     { name: 'Text', route: '/text' },
   ];
+    testNavItems = [
+        { name: 'Status', route: 'status' },
+        { name: 'AAA Test Pattern', route: 'aaa'},
+        { name: 'Asynchronous Operations', route: 'asynchronous'},
+        { name: 'Configuring Jest', route: 'jest'},
+        { name: 'Triggering Event Handlers', route: 'event-handler'},
+        { name: 'Snapshot Testing', route: 'snapshot'},
+        { name: 'Test Types', route: 'test-types'},
+        { name: 'Resources', route: 'resources'}
+    ];
     selectedOption: string;
-    constructor (public dialog: MatDialog) {
+    constructor (public dialog: MatDialog, router: Router) {
+        this.router$ = router;
     }
 
-  testNavItems = [
-    { name: 'Status', route: 'status' },
-    { name: 'AAA Test Pattern', route: 'aaa'},
-    { name: 'Asynchronous Operations', route: 'asynchronous'},
-    { name: 'Configuring Jest', route: 'jest'},
-    { name: 'Triggering Event Handlers', route: 'event-handler'},
-    { name: 'Snapshot Testing', route: 'snapshot'},
-    { name: 'Test Types', route: 'test-types'},
-    { name: 'Resources', route: 'resources'}
-  ];
+    ngOnInit(): void {
+        this.router$.events.subscribe(route => {
+            if (route instanceof NavigationEnd) {
+                this.currUrl = route.url;
+            }
+        });
+    }
 
     openDialog(): void {
-        let dialogRef = this.dialog.open(SlackBotDialogComponent);
+        let dialogRef = this.dialog.open(SlackBotDialogComponent, {
+            data: { url: this.currUrl, baseUrl: 'http://ignite-design-system.apps.mia.ulti.io/#' },
+        });
         dialogRef.afterClosed().subscribe(result => {
             this.selectedOption = result;
         });
@@ -74,26 +87,30 @@ export class AppComponent {
     selector: 'slackbot-dialog',
     templateUrl: './slackbot-dialog.html',
 })
+
 export class SlackBotDialogComponent implements OnInit, OnDestroy {
 
     public formBuilder: FormBuilder;
     private close$ = new Subject<void>();
     form: FormGroup;
 
-    constructor (formBuilder: FormBuilder) {
+    constructor (formBuilder: FormBuilder, @Inject(MAT_DIALOG_DATA) private data: any, private slack: SlackService) {
         this.formBuilder = formBuilder;
     }
     ngOnInit(): void {
         this.form = this.formBuilder.group({
-            myDateRange: this.formBuilder.group({
-                startDate: [null],
-                endDate: [null]
-            })
-        })
+            name: [],
+            question: [],
+        });
     }
 
     ngOnDestroy(): void {
         this.close$.next();
         this.close$.complete();
+
+    }
+    passInformation() {
+        this.slack.sendMessage(this.data.baseUrl + this.data.url, this.data.url.substring(1),
+            this.form.get('name').value, this.form.get('question').value).subscribe();
     }
 }
---
+++ demo-app/app/services/slack-service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+@Injectable()
+export class SlackService {
+    constructor (private http: HttpClient) {
+    }
+
+    sendMessage ( completeUrl, component, user, question ) {
+        const url = 'https://hooks.slack.com/services/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';
+        const body: any = {
+            'text': `New Component Question from <@${user}>!`,
+            'attachments': [
+                {
+                    'title': `Question for Component: ${component}`,
+                    'text': `${question}`,
+                    'color': `#3AA3E3`
+                },
+                {
+                    'title': `Component page`,
+                    'text': `< ${completeUrl} | ${component} >`
+                }
+            ]
+        };
+        const options: {
+            headers?: HttpHeaders,
+            observe?: 'body',
+            params?: HttpParams,
+            reportProgress?: boolean,
+            responseType: 'text',
+            withCredentials?: boolean
+        } = {
+            responseType: 'text'
+        };
+        const payload = JSON.stringify(body);
+        return this.http.post(url, payload, options);
+    }
+}
