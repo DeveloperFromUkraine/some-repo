333a72c724a5d9fe78dc702134a1b53a6ce780b1
Brian Mahecha <mahebd@mail.broward.edu>
2018-02-12 08:45:18 -0500

    cleaned up TypeErrors

--- demo-app/app/testing/components/status/status.component.ts
+++ demo-app/app/testing/components/status/status.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterContentInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Apollo } from "apollo-angular/Apollo";
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 import { Service, Branches, Values, Builds } from "../../models/service";
 import gql from 'graphql-tag';
+
+import fetchServiceInfo from '../../gql/fetchServiceInfo';
 
 @Component({
     templateUrl: './status.component.html',
@@ -24,72 +26,9 @@ export class StatusComponent implements OnInit {
         this.getServiceInfo();
     }
 
-    getBorder() {
-        //return `10px solid ${this.data.metrics.status.toLowerCase()}`;
-    }
-
     getServiceInfo() {
         this.apollo.watchQuery<any>({
-            query: gql`
-                query serviceBySlug($slug: String!) {
-                    serviceBySlug(slug: $slug) {
-                        name
-                        metrics {
-                            status
-                            greenSinceDate
-                            lastFailure {
-                                job
-                                time
-                                author
-                                branchName
-                                concourseUrl
-                                branchType
-                                status
-                                commitMessage
-                                commitHash
-                            }
-                            lastSuccess {
-                                job
-                                time
-                                author
-                                branchName
-                                concourseUrl
-                                branchType
-                                status
-                                commitMessage
-                                commitHash
-                            }
-                        }
-                        project
-                        repoUrl
-                        branches(types: [MASTER, DEVELOP]) {
-                            values {
-                                displayId
-                                url
-                                latestCommit {
-                                    author {
-                                        displayName
-                                        emailAddress
-                                        avatarUrl
-                                    }
-                                    message
-                                }
-                                builds(limit: 5) {
-                                    author
-                                    time
-                                    branchName
-                                    concourseUrl
-                                    branchType
-                                    duration
-                                    pipeline
-                                    status
-                                    commitMessage
-                                }
-                            }
-                        }
-                    }
-                }
-            `,
+            query: fetchServiceInfo,
             variables: {
                 slug: this.service
             }
