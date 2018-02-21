9359ee0d49d790c4a953df6fcde62823128fa19d
Brian Mahecha <mahebd@mail.broward.edu>
2018-02-21 08:53:18 -0500

    built graphql query to get branch info needed for landing page

--- demo-app/app/testing/gql/fetchBranchInfo.ts
+++ demo-app/app/testing/gql/fetchBranchInfo.ts
@@ -3,57 +3,13 @@ import gql from 'graphql-tag';
 const fetchBranchInfo = gql`
                 query serviceBySlug($slug: String!) {
                     serviceBySlug(slug: $slug) {
-                        name
-                        metrics {
-                            status
-                            greenSinceDate
-                            lastFailure {
-                                job
-                                time
-                                author
-                                branchName
-                                concourseUrl
-                                branchType
-                                status
-                                commitMessage
-                                commitHash
-                            }
-                            lastSuccess {
-                                job
-                                time
-                                author
-                                branchName
-                                concourseUrl
-                                branchType
-                                status
-                                commitMessage
-                                commitHash
-                            }
-                        }
-                        project
-                        repoUrl
                         branches(types: [MASTER, DEVELOP]) {
                             values {
-                                displayId
-                                url
-                                latestCommit {
-                                    author {
-                                        displayName
-                                        emailAddress
-                                        avatarUrl
-                                    }
-                                    message
                                 }
                                 builds(limit: 5) {
-                                    author
-                                    time
                                     branchName
                                     concourseUrl
-                                    branchType
-                                    duration
-                                    pipeline
                                     status
-                                    commitMessage
                                 }
                             }
                         }
