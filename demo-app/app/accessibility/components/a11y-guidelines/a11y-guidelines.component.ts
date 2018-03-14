f1aa5855afb6d575b8ca0892de50c354518e4b41
Daniel Weisberg <danielwe@a07457danielwe.us.corp>
2018-03-14 13:58:34 -0400

    Added accessibility components links to a11y landing page

--- demo-app/app/accessibility/components/a11y-guidelines/a11y-guidelines.component.ts
+++ demo-app/app/accessibility/components/a11y-guidelines/a11y-guidelines.component.ts
@@ -3,13 +3,20 @@ import { Router } from '@angular/router';
 
 
 @Component({
-    selector: 'accessibility-guidelines-component',
+    selector: 'accessibility-guidelines',
     templateUrl: './a11y-guidelines.html',
 })
 export class AccessibilityGuidelinesComponent {
     private router$: Router;
 
     componentList = [
+        { name: 'Checkboxes', route: '/checkbox' },
+        { name: 'Error Banner', route: '/error-banner' },
+        { name: 'Floating Action Button', route: '/expandable-fab' },
+        { name: 'Icon', route: '/icon' },
+        { name: 'Info Banner', route: '/info-banner' },
+        { name: 'Loading Container', route: '/loading-container' },
+        { name: 'Nav List', route: '/nav-list' },
         { name: 'Radio Buttons', route: '/radio-button' },
     ];
 
