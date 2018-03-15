36297c3fc873030c298a75bf8e5ce0ebe985c02d
Brian Mahecha <mahebd@mail.broward.edu>
2018-03-15 16:03:07 -0400

    sub test

--- demo-app/app/gql/subscriptionRepositoryChanged.ts
+++ demo-app/app/gql/subscriptionRepositoryChanged.ts
@@ -1,50 +1,51 @@
 import gql from 'graphql-tag';
 
 export const SubscriptionRepositoryChanged = gql`
-repositoryChanged(types: $types, project: $project, repo: $repo, branches: $branches) {
-    type
-    repo {
-      slug
-      id
-      name
-      state
-      project
-      url
-      branches(types: [MASTER, DEVELOP]) {
-        values {
-          displayId
-          url
-          latestCommit {
-            author {
-              displayName
-              emailAddress
-              avatarUrl
+    subscription($types: [RepoChangeType], $project: String!, $repo: String!, $branches: [String]) {
+        repositoryChanged(types: $types, project: $project, repo: $repo, branches: $branches) {
+            type
+            repo {
+              slug
+              id
+              name
+              state
+              project
+              url
+              branches(types: [MASTER, DEVELOP]) {
+                values {
+                  displayId
+                  url
+                  latestCommit {
+                    author {
+                      displayName
+                      emailAddress
+                      avatarUrl
+                    }
+                    message
+                  }
+                  builds(limit: 5) {
+                    author
+                    time
+                    branchName
+                    concourseUrl
+                    branchType
+                    duration
+                    pipeline
+                    status
+                    commitMessage
+                  }
+                }
+              }
             }
-            message
-          }
-          builds(limit: 5) {
-            author
-            time
             branchName
-            concourseUrl
-            branchType
-            duration
-            pipeline
-            status
-            commitMessage
+            fromHash
+            from {
+              id
+            }
+            toHash
+            to {
+              id
+            }
           }
-        }
-      }
-    }
-    branchName
-    fromHash
-    from {
-      id
-    }
-    toHash
-    to {
-      id
-    }
-  }
     }
 `;
