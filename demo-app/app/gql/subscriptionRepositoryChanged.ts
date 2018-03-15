38290cf3d0739ebcc3fce0481d0f16d21ed2f9c9
Brian Mahecha <mahebd@mail.broward.edu>
2018-03-15 16:30:27 -0400

    sub test

--- demo-app/app/gql/subscriptionRepositoryChanged.ts
+++ demo-app/app/gql/subscriptionRepositoryChanged.ts
@@ -4,17 +4,7 @@ export const SubscriptionRepositoryChanged = gql`
     subscription($types: [RepoChangeType], $project: String!, $repo: String!, $branches: [String]) {
         repositoryChanged(types: $types, project: $project, repo: $repo, branches: $branches) {
             type
-            repo {
-                branches(types: [MASTER, DEVELOP, FEATURE]) {
-                    values {
-                        builds(limit: 1) {
-                            branchName
-                            concourseUrl
-                            status
-                        }
-                    }
-                }
-            }
+            
         }
     }
 `;
