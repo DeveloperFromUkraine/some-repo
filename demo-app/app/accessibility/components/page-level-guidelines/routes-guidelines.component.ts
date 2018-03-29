0e824f85eb716dd308d1fca6081af06c7506a40c
Daniel Weisberg <danielwe@a07457danielwe.us.corp>
2018-03-29 13:27:22 -0400

    Added accessibility documentation for routes and titles

---
+++ demo-app/app/accessibility/components/page-level-guidelines/routes-guidelines.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'accessibility-routes-guidelines',
+    templateUrl: './routes-guidelines.html',
+})
+export class AccessibilityRoutesGuidelinesComponent {}
