bb2d786093e7b41d462bb2974ce78012b49e2002
Mariah <mariah_avalos@ultimatesoftware.com>
2018-04-13 14:54:50 -0400

    removing the playground from ids

--- demo-app/app/models/index.ts
+++
@@ -1,4 +0,0 @@
-export { JestBuild, SnapShot, TestResults, AssertionResults } from './jestBuild';
-export { Coverage, Detail } from './coverage';
-export { Service, Branches, Values, Builds } from './service'
-export { Branch } from './branch';
