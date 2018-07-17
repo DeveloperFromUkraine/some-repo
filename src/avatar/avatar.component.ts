4bf21c1f24456cb29678ad5b98c06e1da24c296b
Andrew Ferguson <Andrew_Ferguson@ultimatesoftware.com>
2018-07-17 16:00:32 -0400

    Added enum with new avatar sizes. Added ng_pkg_build to .gitignore

--- src/avatar/avatar.component.ts
+++ src/avatar/avatar.component.ts
@@ -1,23 +1,17 @@
 import { Component, Input } from '@angular/core';
 
+enum AvatarSize {
+  XS = 'xs',
+  S = 's',
+  M = 'm',
+  L = 'l',
+  XL = 'xl',
+  XXL = 'xxl',
+}
+
 @Component({
   selector: 'ign-avatar',
-  template: `
-    <div id="avatar"
-      [class]="'container ' + getSize(size)"
-      matTooltip="{{ tooltip }}"
-      attr.aria-label="{{ tooltip || ariaLabel }}"
-      tabindex="{{ (!!tooltip || !!ariaLabel) ? 0 : -1 }}"
-    >
-      <img *ngIf="!!image" [src]="image" class="image" alt="">
-      <div *ngIf="!image && !!initials" class="initials-container">
-        <span class="initials" aria-hidden="true">{{ initials }}</span>
-      </div>
-      <div *ngIf="!image && !initials" class="icon-container">
-        <mat-icon class="icon" aria-hidden="true">person</mat-icon>
-      </div>
-    </div>
-  `,
+  templateUrl: './avatar.html',
   styleUrls: ['./avatar.component.scss'],
 })
 export class AvatarComponent {
@@ -28,11 +22,6 @@ export class AvatarComponent {
   @Input() ariaLabel: string;
 
   getSize(size: string): string {
-    const sizes = {
-      small: 'small',
-      large: 'large',
-      default: 'large',
-    };
-    return sizes[size] || sizes['default'];
+    return AvatarSize[size] || AvatarSize.L;
   }
 }
