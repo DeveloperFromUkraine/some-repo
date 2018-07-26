d07d370597057ed02d3a2618090dd7db8bb76bc8
Andrew Ferguson <Andrew_Ferguson@ultimatesoftware.com>
2018-07-26 10:20:41 -0400

    added support for old avatar sizes

--- src/avatar/avatar.component.ts
+++ src/avatar/avatar.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 
 enum AvatarSize {
+  small = 's',
+  large = 'l',
+  medium = 'm',
   XS = 'xs',
   S = 's',
   M = 'm',
@@ -8,17 +11,27 @@ enum AvatarSize {
   XL = 'xl',
 }
 
+const deprecatedSizees = ['small', 'large'];
+
 @Component({
   selector: 'ign-avatar',
   templateUrl: './avatar.html',
   styleUrls: ['./avatar.component.scss'],
 })
-export class AvatarComponent {
+export class AvatarComponent implements AfterViewInit {
   @Input() image: string;
   @Input() initials: string;
   @Input() size: string;
   @Input() tooltip: string;
   @Input() ariaLabel: string;
+
+  ngAfterViewInit() {
+    if (deprecatedSizees.includes(this.size)) {
+      console.warn(
+        'Deprecation warning: Avatar sizes "small", "medium" and "large" are being removed in favor of "s", "m" and "l" respectively. Support for these sizes will be dropped soon.'
+      );
+    }
+  }
 
   getSize(size: string): string {
     return AvatarSize[size.toUpperCase()] || AvatarSize.L;
