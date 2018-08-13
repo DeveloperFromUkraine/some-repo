d2941d7ed21d58cf4d691d21d5f6f3484bf2079c
Josh Wang <josh_wang@ultimatesoftware.com>
2018-08-13 10:41:11 -0400

    Renamed avatar component template file

--- src/avatar/avatar.component.ts
+++ src/avatar/avatar.component.ts
@@ -15,7 +15,7 @@ const deprecatedSizees = ['small', 'large', 'medium'];
 
 @Component({
   selector: 'ign-avatar',
-  templateUrl: './avatar.html',
+  templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
 })
 export class AvatarComponent implements AfterViewInit {
