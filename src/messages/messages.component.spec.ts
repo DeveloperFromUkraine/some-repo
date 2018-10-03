e919e81234768c801d9badc80b7f3943a8d50b90
Andrew Ferguson <Andrew_Ferguson@ultimatesoftware.com>
2018-10-03 16:12:30 -0400

    added stylelint rules

--- src/messages/messages.component.spec.ts
+++ src/messages/messages.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ComponentTest } from '../../test/test-bed/component';
 import { MessagesComponent } from './messages.component';
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { Logger } from '../logging/logger.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -9,10 +9,11 @@ describe('Messages', () => {
   let fixture: ComponentFixture<MessagesComponent>;
 
   beforeEach(() => {
-    ComponentTest.createTestBed([], [MessagesComponent] as Component[], [
-      Logger,
-      { provide: ActivatedRoute, useValue: {} },
-    ]);
+    ComponentTest.createTestBed(
+      [],
+      [MessagesComponent] as Component[],
+      [Logger, { provide: ActivatedRoute, useValue: {} }] as Injectable[]
+    );
 
     fixture = TestBed.createComponent(MessagesComponent);
   });
