12a155e540f504e9b0cb7f05ccd2628d3fad76b9
Mike Brocchi <mbrocchi@gmail.com>
2018-11-20 17:47:37 -0500

    Update avatar component module & spec

--- projects/ign/src/lib/avatar/avatar.module.ts
+++ projects/ign/src/lib/avatar/avatar.module.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AvatarComponent } from './avatar.component';
 import { MatTooltipModule, MatIconModule } from '@angular/material';
 
 @NgModule({
-  imports: [ MatIconModule, MatTooltipModule ],
+  imports: [ CommonModule, MatIconModule, MatTooltipModule ],
   declarations: [ AvatarComponent ],
-  exports: [ AvatarComponent ]
+  exports: [ CommonModule, AvatarComponent ]
 })
 export class AvatarModule {}
