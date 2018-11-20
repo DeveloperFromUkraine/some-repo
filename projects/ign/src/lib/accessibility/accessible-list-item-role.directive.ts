385184f28853384fc2d9d8f4926351efeac6614b
Maham Boghani <maham_boghani@ultimatesoftware.com>
2018-11-20 14:09:26 -0500

    Moving business logic out of the constructor for a11y directives

--- projects/ign/src/lib/accessibility/accessible-list-item-role.directive.ts
+++ projects/ign/src/lib/accessibility/accessible-list-item-role.directive.ts
@@ -1,11 +1,13 @@
-import {Attribute, Directive, ElementRef, Renderer2} from '@angular/core';
+import {AfterContentInit, Directive, ElementRef, Renderer2} from '@angular/core';
 
 @Directive({
   /* tslint:disable-next-line */
   selector: 'mat-list-item, ign-list-item, ign-list-selection-item',
 })
-export class AccessibleListItemRoleDirective {
-  constructor(@Attribute('role') role: string, renderer: Renderer2, elementRef: ElementRef) {
-    renderer.setAttribute(elementRef.nativeElement, 'role', 'listitem');
+export class AccessibleListItemRoleDirective implements AfterContentInit {
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+
+  ngAfterContentInit() {
+    this.renderer.setAttribute(this.elementRef.nativeElement, 'role', 'listitem');
   }
 }
--- projects/ign/src/lib/accessibility/accessible-tooltip.directive.ts
+++ projects/ign/src/lib/accessibility/accessible-tooltip.directive.ts
@@ -1,19 +1,20 @@
-import {Attribute, Directive, ElementRef, Renderer2} from '@angular/core';
+import {AfterContentInit, Attribute, Directive, ElementRef, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[matTooltip]',
 })
-export class AccessibleTooltipDirective {
+export class AccessibleTooltipDirective implements AfterContentInit {
   constructor(
-    @Attribute(`matTooltip`)
-        matTooltip: string,
-    @Attribute('aria-label') ariaLabel: string,
-    renderer: Renderer2,
-    elementRef: ElementRef
-  ) {
-    if (matTooltip && !ariaLabel) {
-      renderer.setAttribute(elementRef.nativeElement, `aria-label`, matTooltip);
-      renderer.setAttribute(elementRef.nativeElement, 'tabindex', '0');
+    @Attribute(`matTooltip`) private matTooltip: string,
+    @Attribute('aria-label') private ariaLabel: string,
+    private renderer: Renderer2,
+    private elementRef: ElementRef
+  ) {}
+
+  ngAfterContentInit() {
+    if (this.matTooltip && !this.ariaLabel) {
+      this.renderer.setAttribute(this.elementRef.nativeElement, `aria-label`, this.matTooltip);
+      this.renderer.setAttribute(this.elementRef.nativeElement, 'tabindex', '0');
     }
   }
 }
