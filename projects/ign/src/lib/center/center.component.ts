9391a62807f7ff8e22dfd3b2d09b392bb75334d1
Maham Boghani <maham_boghani@ultimatesoftware.com>
2018-11-27 12:01:27 -0500

    Deprecating ign-center

--- projects/ign/src/lib/center/center.component.ts
+++ projects/ign/src/lib/center/center.component.ts
@@ -1,9 +1,14 @@
-/* tslint:disable:max-classes-per-file */
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'ign-center',
   templateUrl: './center.html',
   styleUrls: ['./center.scss'],
 })
-export class CenterComponent {}
+export class CenterComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    console.warn(
+      `Deprecation warning: 'ign-center' is being removed in the next release. Support for this component will be dropped soon.`
+    );
+  }
+}
